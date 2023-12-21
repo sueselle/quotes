ad284d9cfe100d10c5938a2df9012c4984bb24f6
sueselle <sue@thehappyuser.com>
2023-12-21 12:00:11 -0800

    removing submodule

---
+++ quotivate/src/App.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Loader } from "react-feather";
+import Quotes from "./components/quotes/Quotes";
+import "./App.css";
+
+function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const quotesUrl =
+    "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(quotesUrl);
+      const results = await response.json();
+      setQuotes(results);
+    } catch (e) {
+      console.log("oops", e);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+  
+  return (
+    <div className='App'>
+      <Header />
+      <main>{loading ? <Loader /> : <Quotes quotes={quotes} />}</main>
+      <Footer />
+    </div>
+  );
+}
+export default App;
