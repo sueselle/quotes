66b1efc6fbedf4a966372f44bf23c59a1a9fa1d4
sueselle <sue@thehappyuser.com>
2023-12-22 07:11:43 -0800

    added filter feature

---
+++ quotivate/src/components/quotes/CategoryForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Filter} from "react-feather";
+
+const CategoryForm = ({categories, category, handleCategoryChange}) => {
+    return (
+    <div className='category-form'>
+  <form className='category-filter' onSubmit={(e) => {
+e.preventDefault();
+}}>
+    <Filter />
+     <label htmlFor='category'>Filter Quotes:</label>
+     <select id='category' name='category'>
+        {categories.map(category => (
+            <option key={category} value={category}>
+                {category}
+            </option>
+        ))}
+     </select>
+  </form>
+</div>
+    );
+};
+
+export default CategoryForm;
