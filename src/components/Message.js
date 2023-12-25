10535b0900ade2557446255795ba6d48a7d9f03e
sueselle <sue@thehappyuser.com>
2023-12-25 08:09:48 -0800

    code clean up for deploy

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,20 +1,23 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
-function Message({messageText, removeMessage}) {
-    useEffect(() => {
-        const messageTimeout = window.setTimeout(() => {
-          removeMessage();
-        }, 1500);
-    
-        return () => window.clearTimeout(messageTimeout);
-      },[]);
-    return (
-        <div className="message">
-        <p>{messageText}
-        </p>
-        <span className="close-message" onClick={removeMessage}>x</span>
-        </div>
-    )
-} 
+const Message = ({ messageText, removeMessage }) => {
+  useEffect(() => {
+    const messageTimeout = setTimeout(() => {
+      removeMessage();
+    }, 1500);
+    window.setTimeout(messageTimeout);
+
+    return () => window.clearTimeout(messageTimeout);
+  });
+  return (
+    <div className='message'>
+      <p>{messageText}</p>
+
+      <span className='close-message' onClick={removeMessage}>
+        x
+      </span>
+    </div>
+  );
+};
 
 export default Message;
